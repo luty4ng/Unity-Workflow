a25f270ed6e14e39e5293a2e415747e91b5c1494
Luty <lutydata@gmail.com>
2022-09-19 20:22:33 +0800

    update

---
+++ Assets/_GameWorkflow/GameKit/Scripts/Editor/Misc/Help.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityGameKit.Editor
+{
+    /// <summary>
+    /// 帮助相关的实用函数。
+    /// </summary>
+    public static class Help
+    {
+        [MenuItem("Game Kit/Documentation", false, 90)]
+        public static void ShowDocumentation()
+        {
+            ShowHelp("https://GameKit.cn/document/");
+        }
+
+        [MenuItem("Game Kit/API Reference", false, 91)]
+        public static void ShowApiReference()
+        {
+            ShowHelp("https://GameKit.cn/api/");
+        }
+
+        private static void ShowHelp(string uri)
+        {
+            Application.OpenURL(uri);
+        }
+    }
+}
---
+++ Assets/_GameWorkflow/GameKit/Scripts/Runtime/Debugger/DebuggerComponent.OperationsWindow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace UnityGameKit.Runtime
+{
+    public sealed partial class DebuggerComponent : GameKitComponent
+    {
+        private sealed class OperationsWindow : ScrollableDebuggerWindowBase
+        {
+            protected override void OnDrawScrollableWindow()
+            {
+                GUILayout.Label("<b>Operations</b>");
+                GUILayout.BeginVertical("box");
+                {
+                    ObjectPoolComponent objectPoolComponent = GameKitCenter.GetComponent<ObjectPoolComponent>();
+                    if (objectPoolComponent != null)
+                    {
+                        if (GUILayout.Button("Object Pool Release", GUILayout.Height(30f)))
+                        {
+                            objectPoolComponent.Release();
+                        }
+
+                        if (GUILayout.Button("Object Pool Release All Unused", GUILayout.Height(30f)))
+                        {
+                            objectPoolComponent.ReleaseAllUnused();
+                        }
+                    }
+
+                    ResourceComponent resourceCompoent = GameKitCenter.GetComponent<ResourceComponent>();
+                    if (resourceCompoent != null)
+                    {
+                        if (GUILayout.Button("Unload Unused Assets", GUILayout.Height(30f)))
+                        {
+                            resourceCompoent.ForceUnloadUnusedAssets(false);
+                        }
+
+                        if (GUILayout.Button("Unload Unused Assets and Garbage Collect", GUILayout.Height(30f)))
+                        {
+                            resourceCompoent.ForceUnloadUnusedAssets(true);
+                        }
+                    }
+
+                    if (GUILayout.Button("Shutdown Game Kit (None)", GUILayout.Height(30f)))
+                    {
+                        GameKitCenter.Shutdown(ShutdownType.None);
+                    }
+                    if (GUILayout.Button("Shutdown Game Kit (Restart)", GUILayout.Height(30f)))
+                    {
+                        GameKitCenter.Shutdown(ShutdownType.Restart);
+                    }
+                    if (GUILayout.Button("Shutdown Game Kit (Quit)", GUILayout.Height(30f)))
+                    {
+                        GameKitCenter.Shutdown(ShutdownType.Quit);
+                    }
+                }
+                GUILayout.EndVertical();
+            }
+        }
+    }
+}
